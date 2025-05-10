07b02847f7a04b265343031e967303f100a1d5ab
狐狸的狐狸画 <740740048@qq.com>
2025-05-11 03:31:45 +0800

    自创自定义图标SVG

--- apps.js
+++ apps.js
@@ -5,7 +5,7 @@ export async function getAppDirectories() {
         {id:'photos',name:'照片',path:'photos/index.html'},
         {id:'calendar',name:'日历',path:'calendar/index.html'},
         {id:'todo',name:'待办事项',path:'todo/index.html'},
-        {id:'todo',name:'测试',path:'test/index.html'},
+        {id:'test',name:'测试',path:'test/index.html'},
         {id:'cloud',name:'云盘',path:'cloud/index.html'}
     ];
 }
